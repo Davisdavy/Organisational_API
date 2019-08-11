b07b1aa3b49b816ed94ab52ad23cc7df956a8723
Davisdavy <davisdvay96@gmail.com>
2019-08-11 10:46:10 +0300

    Complite DAO interface methods

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,27 @@
 package dao;
 
+import models.Departments;
+import models.Employees;
+import models.News;
+
+import java.util.List;
+
 public interface DepartmentsDao {
+    //create
+    void add(Departments departments);
+
+    //list and read all departments
+    List<Departments>getAllDepartments();
+
+    //find and get news of a department
+    Departments findById();
+    List<News>getAllNews(int dpt_id);
+
+    //M:M r-ship
+    void addDptToEmployees(Departments departments, Employees employees);
+    List<Employees> getAllEmployeesBelongingToDepartment(int emp_id);
+
+    //Delete
+    void deleteById(int dpt_id);
+    void clearAll();
 }
